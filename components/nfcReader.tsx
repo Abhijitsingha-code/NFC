ebbee21fedfca4f2ce39d34801dc83fa15900824
abhijit singha <112739356+Abhijitsingha-code@users.noreply.github.com>
2023-11-21 12:12:35 +0530

    Update nfcReader.tsx

--- components/nfcReader.tsx
+++ components/nfcReader.tsx
@@ -7,35 +7,48 @@ const NFCComponent = () => {
   const handleScanClick = async () => {
 
     if ('NDEFReader' in window) {
+      // const ndef = new NDEFReader();
+      // ndef
+      //   .scan()
+      //   .then(() => {
+      //     alert("Scanning.");
+      //     ndef.onreadingerror = (event) => {
+      //       alert("Scan Failed.");
+      //       alert(
+      //         "Error! Cannot read data from the NFC tag. Try a different one?",
+      //       );
+      //       alert(event)
+      //     };
+      //     ndef.onreading = event => {
+      //       alert("Scan started successfully.");
+      //       alert(event.serialNumber)
+      //       const message = event.message;
+      //       for (const record of message.records) {
+      //         alert("Record type:  " + record.recordType);
+      //         alert("MIME type:    " + record.mediaType);
+      //         alert("Record id:    " + record.id);
+      //         const scannedData = message.records.map((record: any) => record.data ? new TextDecoder().decode(record.data) : '').join('');
+      //         setScanData(scannedData);
+      //       }
+      //     };
+      //   })
+      //   .catch((error) => {
+      //     alert(`Error! Scan failed to start: ${error}.`);
+      //   });
+
       const ndef = new NDEFReader();
-      ndef
-        .scan()
-        .then(() => {
-          alert("Scanning.");
-          ndef.onreadingerror = (event) => {
-            alert("Scan Failed.");
-            alert(
-              "Error! Cannot read data from the NFC tag. Try a different one?",
-            );
-            alert(event)
-          };
-          ndef.onreading = event => {
-            alert("Scan started successfully.");
-            alert(event.serialNumber)
-            const message = event.message;
-            for (const record of message.records) {
-              alert("Record type:  " + record.recordType);
-              alert("MIME type:    " + record.mediaType);
-              alert("Record id:    " + record.id);
-              const scannedData = message.records.map((record: any) => record.data ? new TextDecoder().decode(record.data) : '').join('');
-              setScanData(scannedData);
-            }
-          };
-        })
-        .catch((error) => {
-          alert(`Error! Scan failed to start: ${error}.`);
-        });
-    }else{
+      ndef.scan().then(() => {
+        alert("Scan started successfully.");
+        ndef.onreadingerror = () => {
+          alert("Cannot read data from the NFC tag. Try another one?");
+        };
+        ndef.onreading = event => {
+          alert("NDEF message read.");
+        };
+      }).catch(error => {
+        alert(`Error! Scan failed to start: ${error}.`);
+      });
+    } else {
       alert(`NFC not supported.`);
     }
   };
@@ -53,4 +66,3 @@ const NFCComponent = () => {
 };
 
 export default NFCComponent;
-
