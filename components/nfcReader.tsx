9562481214ce93321833e24c146d53cd770aa9e1
abhijit singha <112739356+Abhijitsingha-code@users.noreply.github.com>
2023-11-21 11:21:46 +0530

    Update nfcReader.tsx

--- components/nfcReader.tsx
+++ components/nfcReader.tsx
@@ -9,72 +9,40 @@ const NFCComponent = () => {
 
     try {
       const ndef = new NDEFReader();
-      await ndef.scan();
-      alert("> Scan started");
-
-      ndef.addEventListener("readingerror", () => {
-        alert("Argh! Cannot read data from the NFC tag. Try another one?");
-      });
-
-      ndef.addEventListener("reading", ({ message, serialNumber }:any) => {
-        alert(`> Serial Number: ${serialNumber}`);
-        alert(`> Records: (${message.records.length})`);
-
-        // Assuming the data is a string, you may need to adjust this based on your actual data format
-        const scannedData = message.records.map((record:any) => record.data ? new TextDecoder().decode(record.data) : '').join('');
-        setScanData(scannedData);
-      });
+      ndef
+        .scan()
+        .then(() => {
+          console.log("Scan started successfully.");
+          ndef.onreadingerror = (event) => {
+            console.log(
+              "Error! Cannot read data from the NFC tag. Try a different one?",
+            );
+          };
+          ndef.onreading = ({ event, serialNumber }: any) => {
+            alert(event)
+            alert(serialNumber)
+            const scannedData = event.records.map((record: any) => record.data ? new TextDecoder().decode(record.data) : '').join('');
+            setScanData(scannedData);
+          };
+        })
+        .catch((error) => {
+          console.log(`Error! Scan failed to start: ${error}.`);
+        });
     } catch (error) {
-        alert("Argh! " + error);
+      alert("Argh! " + error);
     }
   };
-
-  const handleWriteClick = async () => {
-    alert("User clicked write button");
-
-    try {
-      const ndef = new NDEFReader();
-      await ndef.write("Hello world!");
-      console.log("> Message written");
-    } catch (error) {
-      console.log("Argh! " + error);
-    }
-  };
-
-  const handleMakeReadOnlyClick = async () => {
-    alert("User clicked make read-only button");
-
-    try {
-      const ndef = new NDEFReader();
-      await ndef.makeReadOnly();
-      console.log("> NFC tag has been made permanently read-only");
-    } catch (error) {
-      console.log("Argh! " + error);
-    }
-  };
-
-  useEffect(() => {
-    // Clean up NFC functionality if needed
-    return () => {
-      // Clean up NFC functionality if needed
-    };
-  }, []);
 
   return (
     <div>
       <button onClick={handleScanClick}>Scan NFC</button>
-      <button onClick={handleWriteClick}>Write to NFC</button>
-      <button onClick={handleMakeReadOnlyClick}>Make Read-Only</button>
 
+      <h2>Scanned Data</h2>
       {scanData && (
-        <div>
-          <h2>Scanned Data</h2>
-          <p>{scanData}</p>
-        </div>
+        <p>{scanData}</p>
       )}
     </div>
   );
 };
 
 export default NFCComponent;
-
