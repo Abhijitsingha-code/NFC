70be4a54c66c41d2ed3919bc1339978b511287f3
abhijit singha <112739356+Abhijitsingha-code@users.noreply.github.com>
2023-11-22 14:41:06 +0530

    Update nfcReader.tsx

--- components/nfcReader.tsx
+++ components/nfcReader.tsx
@@ -1,57 +1,60 @@
 'use client'
 import { useState, useEffect } from 'react';
 
-const NFCComponent = () => {
+const NFCComponent: React.FC = () => {
   const [scanData, setScanData] = useState<string[]>([]);
 
-  const handleScanClick = async () => {
+  useEffect(() => {
+    const handleNFC = async () => {
+      try {
+        const ndef = new NDEFReader();
+
+        ndef.onreading = (event) => {
+          console.log("Scan started successfully.");
+
+          const message = event.message;
+
+          // Process each record in the NDEF message
+          message.records.forEach((record) => {
+            // Accessing common properties of NDEFRecord
+            const recordType = record.recordType;
+            const mediaType = record.mediaType;
+            const id = record.id;
+            const data = new TextDecoder().decode(record.data);
+
+            // Displaying the properties in the console (for debugging purposes)
+            console.log(`Record Type: ${recordType}\nMedia Type: ${mediaType}\nID: ${id}\nData: ${data}`);
+
+            // Save the scan data to the state
+            setScanData((prevScanData) => [...prevScanData, data]);
+          });
+        };
+
+        await ndef.scan();
+      } catch (error) {
+        console.error('Error reading NFC:', error);
+      }
+    };
 
     if ('NDEFReader' in window) {
-      const ndef = new NDEFReader();
-      ndef
-        .scan()
-        .then(() => {
-          alert("Scanning.");
-
-          ndef.onreadingerror = (event) => {
-            alert(`Error! Cannot read data from the NFC tag. Try a different one? ${event}`);
-          };
-
-          ndef.onreading = (event) => {
-            alert("Scan started successfully.");
-            alert(`Serial Number: ${event.serialNumber}`);
-            alert(`Record Data: ${event.message.records.toString()}`);
-            alert(`NDEF Message length: ${event.message.records.length}`);
-
-            event.message.records.forEach((record) => {
-              // Accessing common properties of NDEFRecord
-              const recordType = record.recordType;
-              const mediaType = record.mediaType;
-              const id = record.id;
-              const data = new TextDecoder().decode(record.data);
-  
-              // Displaying the properties in an alert (for debugging purposes)
-              alert(`Record Type: ${recordType}\nMedia Type: ${mediaType}\nID: ${id}\nData: ${data}`);
-  
-              // Save the scan data to the state
-              setScanData((prevScanData) => [...prevScanData, data]);
-            });
-          }
-        })
-        .catch((error) => {
-          alert(`Error! Scan failed to start: ${error}.`);
-        });
+      handleNFC();
+    } else {
+      console.warn('NFC API not supported in this browser.');
     }
-  };
+  }, []);
 
   return (
     <div>
-      <button onClick={handleScanClick}>Scan NFC</button>
-
-      <h2>Scanned Data</h2>
-      <p>{scanData}</p>
+      <h1>Your Next.js NFC Example</h1>
+      <div>
+        <h2>Scan Data:</h2>
+        <ul>
+          {scanData.map((data, index) => (
+            <li key={index}>{data}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
-
 export default NFCComponent;
